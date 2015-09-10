1dc6d7e2a4148dcd404eb20a83cc61a3278beae9
Ryan <rcook184@gmail.com>
2015-09-10 16:23:04 -0400

    Complete change GyroInterface
    -Renamed GyroInterface to AnalogGyro
    -Fixed two problems with the code
    	1. The param sensitivity did not match "sentivity" being passed on
    	2. Fixed an init bug, it is initGyro(), not gyroInit()

---
+++ src/library/gyro/AnalogGyro.java
@@ -0,0 +1,54 @@
+package library.gyro;
+
+import edu.wpi.first.wpilibj.Gyro;
+/**
+ * AnalogGyro
+ * Wrapper class that extends the functionality of gyro, and fixes some issues with it
+ * 
+ * @author Ryan Cook
+ */
+public class AnalogGyro extends Gyro {
+  
+  /**
+   * Constructor for AnalogGyro
+   * @param port sets the port of the gyro
+   */
+  public AnalogGyro(int port) {
+    super(port);
+  }
+    /**
+   * Constructor for AnalogGyro
+   * @param port sets the port of the gyro
+   * @param sensitivity sets the sensitivity of the gyro
+   */
+    public AnalogGyro(int port, int sensitivty) {
+    super(port);
+    super.setSensitivity(sensitivty);
+  }
+  
+    public AnalogGyro(int port, int sensitivty, PIDSourceParameter source) {
+      super(port);
+      super.setSensitivity(sensitivty);
+      super.setPIDSourceParameter(source);
+  }
+  
+  public void init() {
+    super.initGyro();
+  }
+  
+  public void reset() {
+    super.reset();
+  }
+  
+  public double getAngle() {
+    return Math.round(super.getAngle());
+  }
+  
+  public double getRate() {
+    return super.getRate();
+  }
+  
+  public double pidGet() {
+    return super.pidGet();
+  }
+}
